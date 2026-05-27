f500939ec174c90a5d4746fd9ed3ccf60e85665c
Maik Waldboth <maik.waldboth@guidecom.de>
2026-05-27 19:28:05 +0200

    Initial Setup

---
+++ src/main/java/de/dreamworld/mod/DreamworldClient.java
@@ -0,0 +1,10 @@
+package de.dreamworld.mod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DreamworldClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
